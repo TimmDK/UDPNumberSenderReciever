b088d9f9e9f8e185c0c31e6327109225c2227008
Timm Oleander Nielsen <timm0192@edu.easj.dk>
2020-04-03 10:31:18 +0200

    Add project files.

---
+++ UDPNumberSender/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UDPNumberSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
